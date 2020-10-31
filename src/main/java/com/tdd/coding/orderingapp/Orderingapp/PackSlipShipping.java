52645853a4660f245dd74236be68588b8ddd4073
Kazi <kaziabdurrahim1990@gmail.com>
2020-10-31 21:24:39 +0100

    Refactoring PackSlipShipping

--- src/main/java/com/tdd/coding/orderingapp/Orderingapp/PackSlipShipping.java
+++ src/main/java/com/tdd/coding/orderingapp/Orderingapp/PackSlipShipping.java
@@ -3,9 +3,14 @@ package com.tdd.coding.orderingapp.Orderingapp;
 import com.add.coding.orderingapp.interfaces.Packslip;
 
 public class PackSlipShipping implements Packslip {
-
+ private boolean isSlipGenerated;
+ 
+	public void setSlipGenerated(boolean isSlipGenerated) {
+	this.isSlipGenerated = isSlipGenerated;
+}
+@Override
 	public boolean isSlipGenerated() {
-		return true;
+		return isSlipGenerated;
 	}
 
 }
